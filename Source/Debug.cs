f0035b5d15409cdbbc57f1c33ce6e82b56068f2a
CWMlolzlz <CWMlolzlz@gmail.com>
2015-07-14 21:41:53 +1200

    Created. Stores debugging related tools

---
+++ Source/Debug.cs
@@ -0,0 +1,19 @@
+﻿using ColossalFramework.Plugins;
+
+namespace ChallengesMod
+{
+	public class Debug{
+
+		public static readonly bool DEBUG = true;
+		public static void PrintMessage(object msg){
+			if (DEBUG && msg != null){
+				ForcePrintMessage(msg.ToString());
+			}
+		}
+		public static void ForcePrintMessage(object msg){
+			DebugOutputPanel.AddMessage (PluginManager.MessageType.Message, msg.ToString());
+		}
+
+	}
+}
+
